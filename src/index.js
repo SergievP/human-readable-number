34da773e4d13e8b7dd639095ac1f224d09fabefb
Vlad <efremov.vlad.ser@gmail.com>
2023-06-12 16:10:02 +0600

    init: start project, add functional for 0-19 numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function toReadable (number) {
-  
-}
+  let a = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve"];
+  let b = ["twen", "thir", "four", "fift", "six", "seven", "eigh", "nine"];
+    if (number / 13 < 1) {
+        return a[number];
+
+    } else if (number / 20 < 1) {
+      return b[number - 12] + 'teen';
+  }
+  }
