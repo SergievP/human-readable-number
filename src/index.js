00aaac16e5a069eb4180e413404800602fd24c40
Vlad <efremov.vlad.ser@gmail.com>
2023-09-14 17:10:12 +0600

    refactor: optimize the code structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-module.exports = function toReadable (number) {
-  let a = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve"];
-  let b = ["twen", "thir", "four", "fift", "six", "seven", "eigh", "nine"];
-    if (number / 13 < 1) {
-        return a[number];
+module.exports = function toReadable(number) {
+    const singleDigits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const twoDigitsTeen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    } else if (number / 20 < 1) {
-      return b[number - 12] + 'teen';
-  }
-  }
+    if (number === 0) {
+        return 'zero';
+    }
+
+    const numStr = String(number).split('').reverse();
+
+    let result = '';
+
+    if (numStr[2]) {
+        result += singleDigits[numStr[2]] + ' hundred';
+    }
+
+    if (numStr[1] === '1') {
+        result += ' ' + twoDigitsTeen[numStr[0]];
+    } else {
+        if (numStr[1]) {
+            result += ' ' + tens[numStr[1]];
+        }
+
+        if (numStr[0] !== '0') {
+            result += ' ' + singleDigits[numStr[0]];
+        }
+    }
+
+    return result.trim();
+}
